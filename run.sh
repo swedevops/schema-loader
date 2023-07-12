5493afbe4597e38a0c1377d3953d9d21f2b206b7
swedev99 <swedevops99@gmail.com>
2023-07-12 22:56:03 +0530

    Jenkins pipeline concepts

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ git clone https://github.com/swedevops/${COMPONENT}
 cd $COMPONENT
 
 if [ "$SCHEMA_TYPE" == "mongo" ]; then
-  DOCDB_ENDPOINT=$(aws ssm get-parameters  --names prod.docdb.db_endpoint --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
-  DOCDB_USER=$(aws ssm get-parameters  --names prod.docdb.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
-  DOCDB_PASS=$(aws ssm get-parameters  --names prod.docdb.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  DOCDB_ENDPOINT=$(aws ssm get-parameters  --names dev.docdb.db_endpoint --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  DOCDB_USER=$(aws ssm get-parameters  --names dev.docdb.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  DOCDB_PASS=$(aws ssm get-parameters  --names dev.docdb.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
   curl -L https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem -o rds-combined-ca-bundle.pem
   mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER} --password ${DOCDB_PASS} <schema/${COMPONENT}.js
 
@@ -12,9 +12,9 @@ fi
 
 if [ "$SCHEMA_TYPE" == "mysql" ]; then
 
-  RDS_ENDPOINT=$(aws ssm get-parameters  --names prod.rds.db_host --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
-  RDS_USER=$(aws ssm get-parameters  --names prod.rds.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
-  RDS_PASS=$(aws ssm get-parameters  --names prod.rds.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  RDS_ENDPOINT=$(aws ssm get-parameters  --names dev.rds.db_host --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  RDS_USER=$(aws ssm get-parameters  --names dev.rds.db_user --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  RDS_PASS=$(aws ssm get-parameters  --names dev.rds.db_pass --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
   mysql -h ${RDS_ENDPOINT}  -u${RDS_USER} -p${RDS_PASS} < schema/${COMPONENT}.sql
 fi
 
